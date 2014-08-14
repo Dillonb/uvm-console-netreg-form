dc411c4e387bc4d82cee30a1ddc3e71e5fc8bb69
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-08-14 16:06:21 -0400

    Fixed a lot of bugs, added fancy scrolling, finalized in preparation for launch.

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/netregform.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
 
     <!--[if lt IE 9]>
         <script src="js/html5shiv.min.js"></script>
@@ -42,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
     <body>
         <div class="jumbotron">
             <img src="img/techteam_logo3.png">
-            <h1>Netreg Form</h1>
+            <h1>Network Registration</h1>
         </div>
         <div class="container">
             <form role="form" action="index.php" method="post" class="form-horizontal netregform container">
@@ -50,8 +51,22 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
                     <div class="col-sm-1">
                         <h1>1.</h1>
                     </div>
+                    <div class="col-sm-11">
+                        <h3>Notice: This form will <em>ONLY</em> allow you to connect your game or media console through a <em>WIRED</em> connection.</h3>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-5"> </div>
+                    <div class="col-sm-7">
+                        <a class="btn btn-default btn-lg" id="btnWiredAgree">I understand</a>
+                    </div>
+                </div>
+                <div class="form-group" id="groupSelectConsole">
+                    <div class="col-sm-1">
+                        <h1>2.</h1>
+                    </div>
                     <div class="col-sm-4">
-                        <label class="control-label" for="inputConsoleType">Type of Console (Xbox One, Apple TV, PS4, etc)</label>
+                        <label class="control-label" for="inputConsoleType">Pick your console from the list to the right.</label>
                     </div>
                     <div class="col-sm-7">
                         <select size="<?php print count($consoles); ?>" class="form-control select-lg" id="inputConsoleType" name="consoletype">
@@ -68,7 +83,7 @@ EOT;
                 </div>
                 <div class="form-group" id="groupOtherConsole">
                     <div class="col-sm-2">
-                        <h1>1.5.</h1>
+                        <h1>2.5.</h1>
                     </div>
                     <div class="col-sm-3">
                         <label class="control-label" for="inputOtherConsole">Please specify.</label>
@@ -78,11 +93,12 @@ EOT;
                     </div>
                 </div>
                 <div class="form-group" id="groupMacAddress">
+                    <div class="alert alert-info col-sm-12" id="machelp"></div>
                     <div class="col-sm-1">
-                        <h1>2.</h1>
+                        <h1>3.</h1>
                     </div>
                     <div class="col-sm-4">
-                        <label class="control-label" for="inputMacAddress"><em>Wired</em> MAC address of the console (see the help below)</label>
+                        <label class="control-label" for="inputMacAddress">Enter the <em>wired</em> MAC address of the console.</label>
                     </div>
                     <div class="col-sm-7">
                         <input type="text" class="form-control input-lg" id="inputMacAddress" name="macaddress">
@@ -91,12 +107,11 @@ EOT;
                     <div class="alert alert-warning col-sm-12" role="alert">The MAC address looks valid, but isn't long enough.</div>
                     <div class="alert alert-danger col-sm-12" role="alert">
                         <p>The MAC address you entered is invalid. MAC addresses consist of six pairs of numbers 0-9 and letters a-f. Call 802-656-2604 for help with this form.
-                        <p id="machelp"></p>
                     </div>
                 </div>
                 <div class="form-group" id="groupSubmitButton">
                     <div class="col-sm-1">
-                        <h1>3</h1>
+                        <h1>4.</h1>
                     </div>
                     <div class="col-sm-4"></div>
                     <div class="col-sm-7">
@@ -177,6 +192,7 @@ mail($to, $subject, $message, $headers);
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/netregform.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
 
     <!--[if lt IE 9]>
         <script src="js/html5shiv.min.js"></script>
@@ -184,11 +200,18 @@ mail($to, $subject, $message, $headers);
     <![endif]-->
     </head>
     <body>
-        <pre>
-<?php
-print $message;
-?>
-        </pre>
+        <div class="jumbotron">
+            <img src="img/techteam_logo3.png">
+            <h1>Network Registration</h1>
+        </div>
+        <div class="container">
+            <div class="page-header">
+                <h1>Request submitted.</h1>
+            </div>
+            <h5>You will receive a confirmation email shortly stating that we have received your request.</h5>
+            <h5>Please allow up to one business day for the process to complete. You will receive a second email when this has occurred.</h5>
+            <h5>Direct all questions to the UVM Tech Team at (802)656-2604.</h5>
+        </div>
     </body>
 </html>
 
--- js/netregform.js
+++ js/netregform.js
@@ -1,30 +1,51 @@
+function stopRKey(evt) {
+    var evt = (evt) ? evt : ((event) ? event : null);
+    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+    if (evt.keyCode == 13) {
+        alert(node.type);
+    }
+    if ((evt.keyCode == 13) && (node.type=="text" || node.type=="select-one"))  {return false;}
+}
+
+document.onkeypress = stopRKey;
+
 jQuery(document).ready(function($) {
         var regex_macaddr = /^(?:[a-fA-F0-9]{2}\W?){6}$/;
         var regex_incomplete_macaddr = /^(?:[a-fA-F0-9]{2}\W?){0,5}[a-fA-F0-9]?$/;
         function setStatus(element, state) {
             var desiredClass = "has-" + state;
             var classes = ["has-error", "has-warning", "has-success"];
+            var changed = false;
 
             for (var i = 0; i <= classes.length; i++) {
                 if (classes[i] != desiredClass) {
-                    if (element.hasClass(classes[i])) { element.removeClass(classes[i]); }
+                    if (element.hasClass(classes[i])) {
+                        element.removeClass(classes[i]);
+                        changed = true;
+                    }
                 }
             }
             if (desiredClass != "has-") {
-                if (!element.hasClass(desiredClass)) { element.addClass(desiredClass); }
+                if (!element.hasClass(desiredClass)) {
+                    element.addClass(desiredClass);
+                    changed = true;
+                }
             }
+            return changed;
         }
         $("#inputConsoleType").change(function() {
                 $("#groupMacAddress").fadeIn("slow");
                 if ($("#inputConsoleType").val() == "other") {
-                    $("p#machelp").html('Insert a message here for when they pick "Other"');
+                    $("#machelp").html('We suggest searching the internet for help with finding the MAC address for your device.');
                     $("#groupOtherConsole").fadeIn("slow");
                 }
                 else {
                     $("#groupOtherConsole").fadeOut("slow");
                     var selectedOption = $("#inputConsoleType option:selected");
-                    $("p#machelp").html('Click <a href="' + selectedOption.data("machelp") + '">here</a> for instructions on finding the MAC address for your ' + selectedOption.text() + '.');
+                    $("#machelp").html('Click <a href="' + selectedOption.data("machelp") + '">here</a> for instructions on finding the MAC address for your ' + selectedOption.text() + '.');
                 }
+                console.log("Calling scrollTo");
+                $.scrollTo($("#groupMacAddress"), 800);
             });
         $("#inputMacAddress").keyup(function() {
                 var macaddr = $("#inputMacAddress").val();
@@ -34,19 +55,32 @@ jQuery(document).ready(function($) {
                 else {
                     if (macaddr.match(regex_incomplete_macaddr)) {
                         // Valid but incomplete MAC address
-                        setStatus($("#groupMacAddress"), "warning");
-                        $("#groupSubmitButton").fadeOut("fast");
+                        if (setStatus($("#groupMacAddress"), "warning")) {
+                            $("#groupSubmitButton").fadeOut("fast");
+                            $.scrollTo($("#groupMacAddress .alert-warning"), 800);
+                        }
                     }
                     else if (macaddr.match(regex_macaddr)) {
                         // Valid MAC address
-                        setStatus($("#groupMacAddress"), "success");
-                        $("#groupSubmitButton").fadeIn("slow");
+                        if (setStatus($("#groupMacAddress"), "success")) {
+                            $("#groupSubmitButton").fadeIn("slow");
+                            console.log("Calling scrollTo");
+                            $.scrollTo($("#groupSubmitButton"), 800);
+                        }
                     }
                     else {
                         // Invalid MAC address
-                        setStatus($("#groupMacAddress"), "error");
-                        $("#groupSubmitButton").fadeOut("fast");
+                        if (setStatus($("#groupMacAddress"), "error")) {
+                            $("#groupSubmitButton").fadeOut("fast");
+                            $.scrollTo($("#groupMacAddress .alert-danger"), 800);
+                        }
                     }
                 }
+            });
+
+        $("#btnWiredAgree").click(function() {
+                $("#groupSelectConsole").fadeIn("slow");
+                console.log("Calling scrollTo");
+                $.scrollTo($('#groupSelectConsole'), 800);
             });
 });
